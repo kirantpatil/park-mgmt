3364788f481a5175541dd74f645839bf9f8348c8
Kiran Patil <kirantpatil@gmail.com>
2015-08-21 18:58:42 +0530

    added image_url to db validation

--- app/models/floor.rb
+++ app/models/floor.rb
@@ -1,5 +1,5 @@
 class Floor < ActiveRecord::Base
-  validates :name, :building_id, :presence => true
+  validates :name, :building_id, :image_url, :presence => true
   has_many :ccunits, dependent: :destroy
   belongs_to :building 
   
