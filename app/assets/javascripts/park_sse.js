9abeb4ff65f3cd87a17a6ec90191b5b3d317440c
Kiran Patil <kiran.patil@fabritronix.com>
2017-05-15 13:03:41 +0530

    Browser refresh for every 30 sec

--- app/assets/javascripts/park_sse.js
+++ app/assets/javascripts/park_sse.js
@@ -134,6 +134,7 @@
     });
 
 var gonfix;
+
 gonfix = function(){
     eval($("#gonvariables > script").html());
 };
@@ -194,6 +195,21 @@ var ready = function() {
     return false;
   });
 };
+
+var time = new Date().getTime();
+
+     $(document.body).bind("mousemove keypress", function(e) {
+         time = new Date().getTime();
+     });
+
+     function refresh() {
+         if(new Date().getTime() - time >= 60000) 
+             window.location.reload(true);
+         else 
+             setTimeout(refresh, 30000);
+     }
+
+     setTimeout(refresh, 30000);
 
 
 $(document).ready(ready);
